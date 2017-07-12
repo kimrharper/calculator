ab501bef3f1a3cd2bac5474b58de184c6cdcd063
Ryan Harper <kimrharper@gmail.com>
2017-07-12 16:07:35 -0700

    Add calculator and message jquery clicks

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+$(function(){
+  $("h1").click(function() {
+    alert("This is a header.");
+  });
+
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+});
+
+/*var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+
+var number2 = parseInt(prompt("Enter a second number:"));
+
+var resultadd = add(number1,number2);
+var resultsubstract = subtract(number1,number2);
+var resultmultiply = multiply(number1,number2);
+var resultdivide = divide(number1,number2);
+
+alert("Results are: Add(" + resultadd + ") Subtract(" + resultsubstract + ") Multiply (" + resultmultiply + ") Divide(" + resultdivide + ")" );
+
+
+var str = 'Twas the night before Xmas...';
+var newstr = str.replace(/xmas/i, 'Christmas');
+alert(newstr);
+*/
